44a91fc6d67f226a2df44fe210f47447f80997d9
Jacek Kowalski <jacek.kowalski@wincor-nixdorf.com>
2018-02-14 22:43:02 +0100

    customization - dynamic content

--- src/app/customization/customization.module.ts
+++ src/app/customization/customization.module.ts
@@ -8,8 +8,7 @@ import {
 } from './dynamic-content/dynamic-content.component';
 import { DynamicContentDemoComponent } from './dynamic-content-demo/dynamic-content-demo.component';
 import { FormsModule } from '@angular/forms';
-import { Extension1Component } from './extension1/extension1.component';
-import { Extension2Component } from './extension2/extension2.component';
+import { extensions } from './extenstions';
 
 @NgModule({
   imports: [
@@ -17,7 +16,7 @@ import { Extension2Component } from './extension2/extension2.component';
     routing,
     FormsModule
   ],
-  declarations: [CustomizationComponent, DynamicContentDemoComponent, DynamicContentComponent, UnknownDynamicComponent, Extension1Component, Extension2Component],
-  entryComponents: [UnknownDynamicComponent, Extension1Component, Extension2Component]
+  declarations: [CustomizationComponent, DynamicContentDemoComponent, DynamicContentComponent, UnknownDynamicComponent, ...extensions],
+  entryComponents: [UnknownDynamicComponent, ...extensions]
 })
 export class CustomizationModule { }
---
+++ src/app/customization/extenstions.ts
@@ -0,0 +1,6 @@
+import { Extension1Component } from './extension1/extension1.component';
+import { Extension2Component } from './extension2/extension2.component';
+
+export const extensions = [
+  Extension1Component, Extension2Component
+];
